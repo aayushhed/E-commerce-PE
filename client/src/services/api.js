a042ebd779f6e9e6babf800cf3e3acd237064f53
aayushhed <ayush121416kumargupta@gmail.com>
2026-06-16 20:32:41 +0530

    Production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://prakash-enterprises-api.onrender.com/api",
 });
 
 export default API;
